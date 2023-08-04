a9f3a3a05f277c2161d60a42b35dc494399c2b07
marioavdullaj <smb995@gmail.com>
2023-08-04 09:36:36 +0200

    General changes and retry mechanisms

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+print("Loading libraries...")
 from PIL import Image, ImageDraw as D
 import cv2
 import numpy as np
@@ -7,6 +8,16 @@ from Mailer import Mailer
 import uuid
 import os
 from dotenv import load_dotenv
+print("DONE!")
+
+def initializeCamera(camera_path):
+    print("Initializing videocamera stream....")
+    if str.isnumeric(camera_path):
+        v = cv2.VideoCapture(int(camera_path))
+    else:
+        v = cv2.VideoCapture(camera_path)
+    print("DONE!")
+    return v
 
 # Global variables
 start_time = datetime.datetime.now()
@@ -15,55 +26,69 @@ sampling_time = 5
 mailing_time = 30
 
 # Environment variables
+print("Loading environment parameters....")
 load_dotenv()
 camera_path = os.getenv('CAMERA_PATH')
 email_address = os.getenv("EMAIL_ACCOUNT")
 app_password = os.getenv("EMAIL_PASSWORD")
 email_receivers = [element.strip() for element in os.getenv("EMAIL_RECEIVERS").strip('[]').split(',')]
+print("DONE!")
 
 # MobileSSD model
 #mobile_ssd_model_path = os.getenv('MOBILE_SSD_MODEL_PATH')
 #dnn = MobileSSDDetector(path_to_ckpt=mobile_ssd_model_path)
 
+print("Loading detection model....")
 # Transformer model
 dnn = DetectorTransformer()
+print("DONE!")
 
 # Mailer class
+print("Mailing handler setup...")
 mailer = Mailer(email_address, app_password, email_address, email_receivers)
+print("DONE!")
 
 # Camera stream capture via opencv
 camera_path = os.getenv('CAMERA_PATH')
 show_camera_preview = os.getenv('SHOW_CAMERA_PREVIEW') == "1"
 
-if str.isnumeric(camera_path):
-    vid = cv2.VideoCapture(int(camera_path))
-else:
-    vid = cv2.VideoCapture(camera_path)
+cameraInitialized = False
+while(not cameraInitialized):
+    try:
+        vid = initializeCamera(camera_path)
+        cameraInitialized = vid is not None
+    except:
+        print("Failed to inizialize the camera! Retrying...")
+        cameraInitialized = False
 
-
+print("The application is running....")
 while(True):
     end_time = datetime.datetime.now()
-    ret, frame = vid.read()
-    if frame is not None:
-        image = cv2.resize(frame, (800,600))
+    try:
+        ret, frame = vid.read()
+        if ret:
+            image = cv2.resize(frame, (800,600))
 
-    if (end_time-start_time).total_seconds() > sampling_time:
-        start_time = end_time
+            if (end_time-start_time).total_seconds() > sampling_time:
+                start_time = end_time
 
-        image, peopleDetected = dnn.processFrame(image)
-        if(show_camera_preview):
-            cv2.imshow("preview", image)
+                image, peopleDetected = dnn.processFrame(image)
+                if(show_camera_preview):
+                    cv2.imshow("preview", image)
 
-        if(peopleDetected):
-            if((end_time-last_time_detected).total_seconds() > mailing_time):
-                if not os.path.exists("./Images"):
-                    os.makedirs("./Images")
-                    
-                image_path = f"./Images/image_{uuid.uuid4()}.jpg"
-                cv2.imwrite(image_path, frame)
+                if(peopleDetected):
+                    if((end_time-last_time_detected).total_seconds() > mailing_time):
+                        if not os.path.exists("./Images"):
+                            os.makedirs("./Images")
+                            
+                        image_path = f"./Images/image_{uuid.uuid4()}.jpg"
+                        cv2.imwrite(image_path, frame)
 
-                mailer.send("Detected people", "People detected at your home!", image_path)
-                last_time_detected = end_time
+                        mailer.send("Detected people", "People detected at your home!", image_path)
+                        last_time_detected = end_time
+    except:
+        print("Failed to capture the frame from the image stream\nReinitializing the videocamera stream...")
+        vid = initializeCamera(camera_path)
 
     key = cv2.waitKey(1)
     if key & 0xFF == ord('q'):
